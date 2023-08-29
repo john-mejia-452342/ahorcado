b02dbc6a6aafe0dfe59f379731412f93325fd06c
unknown <SENA@SGLDFPCATSD254.FormacionSena.RED>
2023-08-29 14:33:36 -0500

    SJAO

--- src/components/facil.vue
+++ src/components/facil.vue
@@ -1,12 +1,20 @@
-<script setup>
-
-</script>
-
 <template>
-  <div>
-    <h1>HI</h1>
+  <div class="container">
+    <div class="container_letras">
+        <div v-for="(item, i) in letras" :key="i">
+            <div><button>{{ item }}</button></div>
+
+        </div>
+    </div>
   </div>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+
+let letras = ref(["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Ñ", "Enviar", "Z", "X", "C", "V", "B", "N", "M","⬅️"])
+</script>
 
 <style scoped>
 .read-the-docs {
